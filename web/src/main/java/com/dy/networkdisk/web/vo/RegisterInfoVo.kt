7c5f52dcf09d5fdeea08224453b4a129a1de3cc6
nic <783415836@qq.com>
2020-03-23 23:02:07 +0800

    2020-03-23 使用kotlin重构项目

--- web/src/main/java/com/dy/networkdisk/web/vo/RegisterInfoVo.kt
+++
@@ -1,8 +0,0 @@
-package com.dy.networkdisk.web.vo
-
-data class RegisterInfoVo (
-        var username : String = "",
-        var password : String = "",
-        var email : String = "",
-        var verificationCode : String = ""
-)
